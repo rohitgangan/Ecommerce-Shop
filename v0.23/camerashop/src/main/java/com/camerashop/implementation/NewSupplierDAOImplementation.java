5345e4c27c4a61c6a2d9499483707beb3ccb5508
ROHIT\DELL <rohitgangan13@gmail.com>
2016-11-22 11:43:30 +0530

    v0.23

---
+++ v0.23/camerashop/src/main/java/com/camerashop/implementation/NewSupplierDAOImplementation.java
@@ -0,0 +1,173 @@
+package com.camerashop.implementation;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.camerashop.model.Cart;
+import com.camerashop.model.Product;
+import com.camerashop.model.Supplier;
+import com.camerashop.model.SupplierActivation;
+import com.camerashop.model.User;
+import com.camerashop.model.UserDetails;
+import com.camerashop.model.UserRole;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+@Repository
+public class NewSupplierDAOImplementation {
+	@Autowired
+	private SessionFactory sessionFactory;
+	public void saveOrUpdateSupplier(UserDetails userDetails) {
+		// TODO Auto-generated method stub
+		Session session = this.sessionFactory.getCurrentSession();
+		
+		User user = new User();
+		/*user.setUserId(userDetails.getUserId());
+		System.out.println("user id is"+userDetails.getUserId());*/
+		user.setUsername(userDetails.getUsername());
+		user.setPassword(userDetails.getPassword());
+		user.setEnabled(false);
+		user.setUserId(userDetails.getUserId());
+		session.saveOrUpdate(user);
+		
+		UserRole userRole = new UserRole();
+		userRole.setUserId(user.getUserId());
+		userRole.setRoleId(2);
+		session.saveOrUpdate(userRole);
+		
+		Cart cart = new Cart();
+		cart.setCartId(user.getUserId());
+		cart.setUserId(user.getUserId());
+		session.saveOrUpdate(cart);
+						
+		
+		
+		userDetails.setRoleId(2);
+		userDetails.setUserId(user.getUserId());
+		userDetails.setCartId(cart.getCartId());
+		
+	
+		
+		/*userDetails.getSupplier().setUserDetails(userDetails);
+		session.saveOrUpdate(userDetails.getSupplier());*/
+		
+		session.saveOrUpdate(userDetails);
+		session.flush();
+		
+	}
+
+	public void activateSupplier(int userId) {
+		// TODO Auto-generated method stub
+		String query = "update User set enabled=true where userId=" +userId;
+		this.sessionFactory.getCurrentSession().createQuery(query).executeUpdate();
+		
+		
+		
+		
+	}
+	
+	
+	public void addSupplierAddress(Supplier supplier){
+		sessionFactory.getCurrentSession().saveOrUpdate(supplier);
+	}
+	
+	
+	@SuppressWarnings("unchecked")
+	public SupplierActivation getById(int userId) {
+		// TODO Auto-generated method stub
+		String query = "from SupplierActivation WHERE userId= " + userId;
+		List<SupplierActivation> supplierActivationList = sessionFactory.getCurrentSession().createQuery(query).getResultList();
+		if(supplierActivationList != null && !supplierActivationList.isEmpty())
+		{
+			
+			return supplierActivationList.get(0);
+		}
+		else
+		{
+		return null;
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<UserDetails> listSupplier() {
+		// TODO Auto-generated method stub
+		List<UserDetails> supplierList = this.sessionFactory.getCurrentSession().createQuery("from UserDetails where roleId=2 and roleId=3").getResultList();
+		return supplierList;
+	}
+	@SuppressWarnings("unchecked")
+	public String stringSupplier() {
+		// TODO Auto-generated method stub
+		List<UserDetails> supplierJList = this.sessionFactory.getCurrentSession().createQuery("from UserDetails").getResultList();
+		Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
+		String supplierGson = gson.toJson(supplierJList);
+		return supplierGson;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public SupplierActivation stringSupplierActivate(){
+		List<SupplierActivation> disabledUser = sessionFactory.getCurrentSession().createQuery("from SupplierActivation").getResultList();
+		//System.out.println(disabledUser.get(0));
+		if(disabledUser != null && !disabledUser.isEmpty())
+		{
+			
+			return disabledUser.get(0);
+		}
+		else
+		{
+		return null;
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public String gsonSupplierActivate(){
+		List<SupplierActivation> supplierJson = this.sessionFactory.getCurrentSession().createQuery("from SupplierActivation").getResultList();
+		Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
+		String sGson = gson.toJson(supplierJson);
+		return sGson;
+		
+	}
+		
+
+	@SuppressWarnings("unchecked")
+	public UserDetails getIdByName(String username) {
+		// TODO Auto-generated method stub
+		String query = "from UserDetails WHERE username= '"+username+"'";
+		List<UserDetails> SupplierList = sessionFactory.getCurrentSession().createQuery(query).getResultList();
+		if(SupplierList != null && !SupplierList.isEmpty())
+		{
+			
+			return SupplierList.get(0);
+		}
+		else
+		{
+		return null;
+		}
+		
+	}
+	@SuppressWarnings("unchecked")
+	public UserDetails getByRoleId() {
+		// TODO Auto-generated method stub
+		String query = "from UserDetails WHERE roleId = '2'";
+		List<UserDetails> SupplierList = sessionFactory.getCurrentSession().createQuery(query).getResultList();
+		if(SupplierList != null && !SupplierList.isEmpty())
+		{
+			
+			return SupplierList.get(0);
+		}
+		else
+		{
+		return null;
+		}
+	}
+	public void delete(int userId) {
+		// TODO Auto-generated method stub
+		UserDetails supplierDelete = new UserDetails();
+		supplierDelete.setUserId(userId);
+		this.sessionFactory.getCurrentSession().delete(supplierDelete);
+	}
+		
+}
---
+++ v0.23/camerashop/src/main/webapp/WEB-INF/views/viewproduct.jsp
@@ -0,0 +1,37 @@
+<%@include file="header.jsp" %>
+<div ng-app="getProductView" ng-controller="productViewController" style="margin-top: 100px;background-color:#f1f1f1;">
+<div class="container-fluid">
+<legend><a href="home">Home</a> >
+<a href="view--{{products.categoryId}}--product">{{products.categoryName}}</a> > 
+<a href="">{{products.subCategoryName}}</a> > 
+{{products.productName}}</legend>
+</div>
+<div class="container-fluid">
+<div class="row">
+<div class="col-sm-12 col-md-7">
+<img src="resources/images/{{products.productId}}.jpg" >
+</div>
+<div class="col-sm-12 col-md-5">
+<li><ul>{{products.productName}}</ul></li>
+<li><ul>{{products.categoryName}}</ul></li>
+<li><ul>Supplier: {{products.supplierCompanyName}}</ul></li>
+</div>
+</div>
+</div>
+<script>
+			var product = ${pGson};
+			angular.module('getProductView', []).controller('productViewController',
+					function($scope) {
+						$scope.products = product;
+						
+
+					});
+		</script>
+		
+	<div style="padding-top:10px">
+
+</div>
+	</div>	
+
+
+<%@include file="footer.jsp" %>
